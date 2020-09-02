764ef5138324369d0ce58a1ec95c6ca7ad84f663
el Aiste <aiste.norei@gmail.com>
2020-09-02 20:25:51 +0300

    up

--- js/data/Services.js
+++ js/data/Services.js
@@ -18,7 +18,7 @@ const data = [
         par: 'It is a long established fact that a reader will be distracted by the  readable content of a page when looking at its layout.',
     }, {
         photo: './img/Services/4.png',
-        name: 'Unlimuted WIfi',
+        name: 'Unlimited WIfi',
         par: 'It is a long established fact that a reader will be distracted by the  readable content of a page when looking at its layout.',
     }, {
         photo: './img/Services/5.png',
